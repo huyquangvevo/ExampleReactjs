6910fa31372e16f996c48e92bc7853d25daec11a
QuangHuy <quanghuy27101997@gmail.com>
2019-09-08 17:48:21 +0700

    Complete Frame

--- src/components/Frame/index.jsx
+++ src/components/Frame/index.jsx
@@ -3,48 +3,63 @@ import * as d3 from 'd3';
 
 class Frame extends Component {
     state = { 
-        
-     };
-
-    projection  = () => {
-        return d3.geoMercator()
-        .scale(100)
-        .translate([800/2,450/2])
+        locations : []
     };
-
     
     componentDidMount(){
-        var svg = d3.select("svg");
-
         d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then(function(data){
             // Draw the map
-            svg.append("g")
+            d3.select("svg").append("g")
                 .selectAll("path")
                 .data(data.features)
                 .enter().append("path")
                     .attr("fill", "#69b3a2")
                     .attr("d", d3.geoPath()
-                        .projection(d3.geoMercator()
-                        .scale(100)
-                        .translate([800/2,450/2]))
+                        .projection(
+                            d3.geoMercator()
+                                .scale(100)
+                                .translate([800/2,450/2])
+                        )
                     )
-                    .style("stroke", "#fff")
+                    .style("stroke", "#fff");
+        })
+        .then(()=>{
+            fetch('https://my-json-server.typicode.com/huyquangvevo/FakeJSON/locations')
+                .then(res => {
+                    return res.json();
+                })
+                .then((result) => {
+                    this.setState({
+                        locations : result
+                    });
+                
+                })
+                .then( () => {
+                    d3.select("svg").append("g")
+                    .selectAll("circle")
+                    .data(this.state.locations)
+                    .enter().append("circle").attr("r",Math.random())
+                    .attr("cx",function(d){return d.x})
+                    .attr("cy",function(d){return d.y})
+                    .attr("stroke","black").attr("strokeWidth",3)
+                    .attr("fill","red");
+                })
+                .then(()=>{
+                    this.handleScale();
+                });
         });
+    }
 
+    handleScale = () =>{
+        d3.selectAll("circle").transition()
+            .duration(7500)
+            .delay(function(d, i) { return i * 10; })
+            .attr("r", function(d) { return Math.sqrt(80); });
     }
 
     render() {
         return ( 
-            <svg width={800} height = {450} viewBox = "0 0 800 450" color="blue">
-               <g fill="white" stroke="green" strokeWidth="5">
-                   <circle
-                    cx = {this.projection()([8,48])[0]}
-                    cy = {this.projection()([8,48])[1]}
-                    r = {10}
-                    fill = "#E91E63"
-                   />
-                    <path d={"M 10 10 H 90 V 90 H 10 L 10 10"}></path>
-                </g>
+            <svg width={1200} height = {450} viewBox = "0 0 800 450" color="blue">
             </svg>
          );
     }
--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import './style.css';
 
 class Menu extends Component {
     state = {  }
     render() { 
         return ( 
-            <div>Hello Huy</div>
+            <div>
+                <div>
+                    <button className="btn btn-warning btn-sm btn-block">Circle</button>
+                </div>
+                <div>
+                    <button className="btn btn-danger btn-sm btn-block">Square</button>
+                </div>
+                <div>
+                    <button className="btn btn-info btn-sm btn-block">Triagle</button>
+                </div>
+            </div>
          );
     }
 }
